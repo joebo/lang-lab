29e9414a4a9c1c42e11fbd4c385553d5d884799a
unknown <joebogner@gmail.com>
2015-02-10 11:02:26 -0500

    more assembly play

--- c/infix.c
+++ c/infix.c
@@ -66,13 +66,13 @@ unsigned int __stdcall  infixThreaded(void *threadData) {
 int add1(int src)
 {
     int dst;
-     
+
      asm ("mov %0, %1\n\t"
          "add $1, %0"
          : "=r" (dst)
           : "r" (src)
           : "memory", "cc" );
-     
+
      printf("add: %d\n", dst);
   return dst;
 }
@@ -81,7 +81,7 @@ int hash2(char *pMem)
 {
     printf("char: %d\n", *pMem);
     printf("ptr: %ld\n", pMem);
-    int dst=0; 
+    int dst=0;
     int infixSize=9;
     /*
     asm ("mov %%eax, 0x1505\n\t"
@@ -121,6 +121,48 @@ int hash2(char *pMem)
 }
 
 
+long fnv(char *buf) {
+    long hval = 0;
+    int len = 9;
+    unsigned char *bp = (unsigned char *)buf;	/* start of buffer */
+    unsigned char *be = bp + len;		/* beyond end of buffer */
+
+    while (bp < be) {
+	hval ^= *bp++;
+
+	hval *= 0x01000193;
+
+    }
+    //printf("fnv hash: %lu\n", hval);
+    return hval;
+}
+
+long hashIt(char *pMem) {
+    long hash = 0;
+    int infixSize = 9;
+    //printf("ptr: %ld\n", pMem);
+    __asm__ __volatile__ (
+                          //"mov $0x811C9DC5, %%eax\n"
+                          "xor %%edi, %%edi \n"
+                          "mov $0x1000193, %%edi \n"
+                          "xor %%esi, %%esi \n"
+                          "xor %%edx, %%edx \n"
+                          "xor %%eax, %%eax \n"
+                          "1: \n"
+                          "movb (%%ebx), %%edx \n"
+                          "xor %%edx, %%eax \n"
+                          "imul %%edi, %%eax \n"
+                          "add $1, %%ebx \n"
+                          //"add %%edx, %%eax \n"
+                          "decb %%ecx \n"
+                          "jnz 1b \n"
+                          : "=a" (hash)
+                          : "b" (pMem),"c" (infixSize)
+                          );
+    //printf("asm hash: %lu\n", hash);
+    return hash;
+}
+
 int count_unique_hash(void* newMem, LONG infixLen, LONG rowCt) {
     char *pMem = newMem;
     //http://www.jsoftware.com/papers/indexof/indexofscript.htm
@@ -128,42 +170,125 @@ int count_unique_hash(void* newMem, LONG infixLen, LONG rowCt) {
     char**table = VirtualAlloc(0, tableSize * sizeof(char*), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
     //adds 2 seconds to initialize
+    /*
     for(LONG i =0; i < tableSize; i++) {
         table[i] = 0;
     }
-    
-    LONG matches=0;
-    LONG collisions=0;
+    */
+    memset(table, 0, tableSize);
+
+    unsigned int matches=0;
+    unsigned int collisions=0;
     for(LONG i = 0; i < rowCt; i++) {
 
-        
+
+
+
+        /*
         unsigned long hash = 5381;
-        
+
         for(int k=0;k<infixLen;k++)
-            hash = (hash + pMem[k]) * 33; // ((hash << 5) + hash) + pMem[k]; 
+            hash = (hash + pMem[k]) * 33; // ((hash << 5) + hash) + pMem[k];
         */
-        unsigned long hash = 0;
-        for(int k=0;k<infixLen;k++) 
-            /* xor the bottom with the current octet */
-            hash ^= pMem[k];
+
+
+
+        /*
+           //26s
+        long hash = 0;
+        unsigned char *p = pMem;
+        unsigned char *be = pMem + 9;
+
+        while (p < be) {
+            hash ^= *p++;
             hash *= 0x01000193;
         }
-        
+        */
+
+
+        unsigned long hash = 0;
+        int infixSize = 9;
+
+        __asm__ __volatile__ (
+            "mov $0x1000193, %%edi \n"
+            "xor %%edx, %%edx \n"
+            "xor %%eax, %%eax \n"
+            "1: \n"
+            "movb (%%ebx), %%edx \n"
+            "xor %%edx, %%eax \n"
+            "imul %%edi, %%eax \n"
+            "add $1, %%ebx \n"
+            "decb %%ecx \n"
+            "jnz 1b \n"
+         : "=a" (hash)
+         : "b" (pMem),"c" (infixSize)
+         );
+
+        //long hash2 = fnv(pMem);
+        //hash = hashIt(pMem);
+        /*
+        long hash3 = fnv(pMem);
+
+        */
+        /*
+        if (hash2 != hash) {
+            for(int k = 0; k < infixLen; k++ ) {
+                printf("%d:%d ", k, pMem[k]);
+            }
+            printf("not equal: %lu %lu\n", hash, hash2);
+        }
+        */
+
         //printf("%lu\n", hash);
         hash = hash % tableSize;
-        //long hash = djb2(pMem, infixLen) % tableSize;
+
+        //matches++;
+        //__asm__ __volatile__ ("add $0x1, %%ecx" : "=c" (matches) : "0" (matches) );
+
+
+        __asm__ __volatile__ (
+                              "imul $0x8, %%esi \n"
+                              "add %%esi, %%edi \n"
+                              "mov $0x0, %%edx \n"
+                              "movb (%%edi), %%edx \n"
+                              "testb $0x1, %%edx \n"
+                              "jne 1f \n"
+                              "add $1, %%eax \n"
+                              "movb $0x1, (%%edi) \n"
+                              "jmp 2f \n"
+                              "1: \n"
+                              "add $1, %%ecx \n"
+                              "2: \n"
+                              //: "=a" (collisions), "=c" (matches), "=D" (table), "c" (hash)
+                              : "=a" (matches), "=c" (collisions)
+                              : "S" (hash), "D" (table), "b" (pMem), "a" (matches), "c" (collisions)
+                                //: "b" (pMem), "0" (collisions), "1" (matches)
+                              : "edx");
+
+
+        //table[hash] = 1;
+        //printf("val: %d\n", matches);
+
+        /*
+        if (matches != 0) {
+            printf("hash: %lu\n", hash);
+            printf("matches: %d\n", matches);
+            printf("col: %d\n", collisions);
+        }
+        */
+
+        /*
         if (table[hash] == 0) {
             table[hash] = pMem;
-           matches++;
+            matches++;
         }
         else {
-            int match = memcmp(pMem, table[hash], infixLen); //strncmp(pMem, table[hash], infixLen);
-           if (match !=0) {
+            int match = -1; // memcmp(pMem, table[hash], infixLen); //strncmp(pMem, table[hash], infixLen);
+           if (match != 0) {
               collisions++;
            }
-           //collisions++;
-            //matches++;
         }
+        */
 
         //printf("key: %d\n", hash);
         pMem+=infixLen;
@@ -257,12 +382,30 @@ int main() {
     long length=ftell(f);
     fseek(f, 0, SEEK_SET);
     char *mem = VirtualAlloc(0, length, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    length= 100000000;
     fread(mem, sizeof(char), length, f);
     long count = 0;
-    //length = 9;
     infix(mem, length, 9, &count);
+
+
     printf("count: %lld\n", count);
+    /*
+    hashIt(mem);
     //add1(10);
-    //hash(mem);
+    strcpy(mem, "abcdefghijklmnop");
+    hashIt(mem);
+    fnv(mem);
+    */
+
+    for (int i=0;i<100000;i++) {
+        strcpy(mem, "NNNNNNN\nN");
+        if (3050835126 != hashIt(mem)) {
+            printf("problem");
+        }
+        strcpy(mem, "foooooooooooooooo");
+        hashIt(mem);
+    }
+    fnv(mem);
+
     fclose(f);
 }
