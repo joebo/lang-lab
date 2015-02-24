2f4917ae6995dbd5e0cd887ec332654014495622
Joe Bogner <joebogner@gmail.com>
2015-02-24 16:36:44 +0000

    add linux example

---
+++ go/http-j/http-j-linux.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"net/http"
+	"runtime"
+        "fmt"
+        "syscall"
+	"unsafe"
+	"io/ioutil"
+)
+/*
+#cgo LDFLAGS: -L.  -lcallj -ldl
+void* jlib_call(char *url, char *body, int *outputLen);
+void jlib_freemem(char *ptr);
+*/
+import "C"
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+
+//hacky
+func bytePtrToString(p uintptr, len int) string {
+	a:=make([]uint8,len+1);
+	for i := 0; i < len; i++ {
+		a[i] = (*[1]uint8)(unsafe.Pointer(p+uintptr(i)))[0]
+	}
+	return string(a[:len])
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	var url = r.URL.RequestURI();
+	urlBytes, _ := syscall.BytePtrFromString(url)
+	var urlPtr = unsafe.Pointer(urlBytes);
+
+	body, _ := ioutil.ReadAll(r.Body)
+
+	bodyBytes, _ := syscall.BytePtrFromString(string(body[:]))
+	var bodyPtr = unsafe.Pointer(bodyBytes)
+	
+	var responseLen uint32 = 0;
+	//ret, _, _ := syscall.Syscall6(uintptr(callJ), 3, uintptr(urlPtr), uintptr(bodyPtr), uintptr(unsafe.Pointer(&responseLen)), 0,0,0)
+	ret, _ :=  C.jlib_call((*C.char)(urlPtr), (*C.char)(bodyPtr), (*C.int)(unsafe.Pointer(&responseLen)));
+
+
+	//TODO: http://stackoverflow.com/questions/19060015/integrating-existing-c-code-to-go-convert-unsigned-char-poiner-result-to-byte
+	var output = bytePtrToString((uintptr)(unsafe.Pointer(ret)), int(responseLen))
+
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, output);
+
+	C.jlib_freemem((*C.char)(ret));
+
+	/*
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "hello world %s %d", body, urlPtr, bodyPtr, responseLen, ret);
+	*/
+}
+
---
+++ go/http-j/libcallj.c
@@ -0,0 +1,108 @@
+//"c:\Users\Joe Bogner\Downloads\tcc\tcc" callj.c j.def -shared -o ..\..\go\http-j\callj.dll
+
+//gcc -shared -fPIC callj.c -L libj.so -o callj.so
+
+#if _WIN64
+#include <windows.h>
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+typedef long long I;
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#if _WIN32
+
+void jlib_bind() { }
+void jlib_close() { }
+#else
+
+#include <dlfcn.h>
+typedef void* (*JInitType)();
+typedef int (*JDoType)(void*, char*);
+typedef int (*JSetMType)(void*, char*, I*, I*, I*, I*);
+typedef int (*JGetMType)(void*, char*, I*, I*, I*, I*);
+typedef void* (*JFreeType)(void*);
+typedef void* (*dllquitType) (void*);
+
+static JInitType JInit = NULL;
+static JDoType JDo = NULL;
+static JSetMType JSetM = NULL;
+static JGetMType JGetM = NULL;
+static JFreeType JFree = NULL;
+static dllquitType dllquit = NULL;
+
+//may need to call dlclose, but not worried about it now
+void* jlib_bind() {
+  void *handle = dlopen("libj.so", RTLD_LAZY);
+  if(!handle) {
+    fputs(dlerror(), stderr);
+    exit(1);
+  }
+  JInit = dlsym(handle, "JInit");
+  JDo = dlsym(handle, "JDo");
+  JSetM = dlsym(handle, "JSetM");
+  JGetM = dlsym(handle, "JGetM");
+  JFree = dlsym(handle, "JFree");
+  dllquit = dlsym(handle, "dllquit");
+
+  return handle;
+}
+
+#endif
+
+DLL_EXPORT char* jlib_call(char *url, char *body, int *outputLen) {
+
+#if _WIN32
+#else
+  if (JInit == NULL) { jlib_bind(); }
+#endif
+
+  void* j  = (void*)JInit();
+
+  
+  I typei=2;
+  I ranki=1;
+  I shapei[1];
+  shapei[0] = (int)strlen(url);
+  void *pshapei = (void*)shapei;
+  void *pdata = (void*)url;
+
+  int ret = JSetM(j, "url_request_", &typei, &ranki, &pshapei, &pdata);
+    
+  shapei[0] = strlen(body);
+  pdata=body;
+  ret = JSetM(j, "body_request_", &typei, &ranki, &pshapei, &pdata);
+  ret = JDo(j, "0!:0 <'server.ijs'");
+  //printf("ret: %d\n", ret);
+
+    
+  int type = 0;
+  int *v = &type;
+  int rank = 0;
+  long *shape;
+  void *pvals = 0;
+  ret = JGetM(j, "response_request_", v, &rank, &shape, &pvals);
+  //printf("ret: %d, type: %d, rank: %d, shape: %d", ret, type, rank, shape[0]);
+    
+  int len = shape[0];
+  char *output = (char*)malloc(sizeof(char)*len);
+  strcpy(output, (char*)pvals);
+  *outputLen = len;
+  printf("\nresponse is: %s\n", output);
+
+  JFree(j);
+
+  return output;
+    
+}
+
+
+DLL_EXPORT void jlib_freemem(char *ptr) {
+  free(ptr);
+}
+
